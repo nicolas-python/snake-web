96f10aaf8287ad6f845e506282173554a9b9d121
Nicolas <nicolasmader001@gmail.com>
2026-06-06 15:15:29 +0200

    feat: add test canvas drawing in game.js

--- static/game.js
+++ static/game.js
@@ -1,1 +1,8 @@
 //Snake Game Logik (Browser)
+
+const canvas = document.getElementById("game");         //zugriff auf das canvas-element aus HTML
+const ctx = canvas.getContext("2d");                                //benutze den 2D-Zeichenstift für das Canvas
+
+// Test: ein Quadrat zeichnen
+ctx.fillStyle = "green";
+ctx.fillRect(100, 100, 20, 20);
