1528457da0defc2af7903e8a51a896648a59b6ab
Nicolas <nicolasmader001@gmail.com>
2026-06-06 14:27:15 +0200

    feat: add first HTML page with Flask template

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)                           #erstellt die flask-app
 @app.route('/')                                 #wenn jemand die Route '/'im Browser aufruft, wird diese Funktion ausgeführt
 def start_page():
-    return "Welcome to Snake"
+    return render_template("index.html")
 
 if __name__ == '__main__':                      #entscheidet ob Programm startet
     app.run(debug=True)                         #sorgt für automatisches Neustarten
