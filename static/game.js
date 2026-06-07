4bbeff2450731b6c5dcad9e8175d2eda55cc273a
Nicolas <nicolasmader001@gmail.com>
2026-06-07 13:43:27 +0200

    feat(game.js): add snake movement with direction control and pause button

--- static/game.js
+++ static/game.js
@@ -8,14 +8,40 @@ const ctx = canvas.getContext("2d");                                //benutze de
 let snakeX = 100;
 let snakeY = 100;
 
+//steuerung
+let direction = "right";
+let paused = false;
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") direction = "left";
+    if (event.key === "ArrowRight") direction = "right";
+    if (event.key === "ArrowUp") direction = "up";
+    if (event.key === "ArrowDown") direction = "down";
+
+    if (event.key === "p") paused = !paused;
+});
+
+//bewegung
+function moveSnake() {
+    if (paused) return;
+
+    if (direction === "right") snakeX += 20;
+    if (direction === "left") snakeX -= 20;
+    if (direction === "up") snakeY -= 20;
+    if (direction === "down") snakeY += 20;
+}
+
+//snake zeichnung(canvas)
 function drawSnake()
 {
     ctx.fillStyle = "green";
     ctx.fillRect(snakeX, snakeY, 20, 20);
 }
 
+//game loop
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);           //clearRect = Bildschirm reset pro Frame
+    moveSnake();
     drawSnake();
 }
 
