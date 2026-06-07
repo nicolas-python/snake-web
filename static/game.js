a6a14b59e8a4fdaf4ac04fc3ccc90bfa1fd648b0
Nicolas <nicolasmader001@gmail.com>
2026-06-07 10:44:00 +0200

    feat(game.js): implement game loop with clear and redraw

--- static/game.js
+++ static/game.js
@@ -1,8 +1,24 @@
 //Snake Game Logik (Browser)
+console.log("game.js geladen");
 
 const canvas = document.getElementById("game");         //zugriff auf das canvas-element aus HTML
 const ctx = canvas.getContext("2d");                                //benutze den 2D-Zeichenstift für das Canvas
 
-// Test: ein Quadrat zeichnen
-ctx.fillStyle = "green";
-ctx.fillRect(100, 100, 20, 20);
+//startpunkt snake
+let snakeX = 100;
+let snakeY = 100;
+
+function drawSnake()
+{
+    ctx.fillStyle = "green";
+    ctx.fillRect(snakeX, snakeY, 20, 20);
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);           //clearRect = Bildschirm reset pro Frame
+    drawSnake();
+}
+
+setInterval(gameLoop, 200);
+
+
